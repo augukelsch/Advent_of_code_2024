52190f0577ec34bd579ad3c012395bf00cd7b0b3
Augusto Kelsch <91554980+augukelsch@users.noreply.github.com>
2024-12-01 18:41:37 -0300

    Resolved Part1

--- 1-1.js
+++ 1-1.js
@@ -2,9 +2,30 @@ const fs = require('fs')
 
 const text = fs.readFileSync('1-1-input.txt').toString().split('\r\n');
 
-console.log(text[1])
+var listLeft = [];
+var listRight = []; 
 
 for(let i = 0; i < text.length; i++){
-
+    var number = text[i].split("   ")
+    var numberLeft = number[0];
+    listLeft.push(numberLeft)
+    var numberRight = number[1];
+    listRight.push(numberRight)
 }
-//console.log(text)
+
+listLeft.sort((a,b)=>a-b);
+listRight.sort((a,b)=>a-b);
+
+var listResult = [];
+
+for(let i=0; i<listLeft.length;i++){
+    result = listLeft[i]-listRight[i];
+    if(result < 0){
+        result = result*-1;
+    }
+    listResult.push(result);
+    console.log(result)
+}
+
+const Result = listResult.reduce((total,a)=> total+a,0)
+console.log(Result);
