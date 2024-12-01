cf04016d070aa750521c408744930ffe21ced862
Augusto Kelsch <91554980+augukelsch@users.noreply.github.com>
2024-12-01 18:21:19 -0300

    Advent_of_code_2024

---
+++ 1-1.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+const text = fs.readFileSync('1-1-input.txt').toString().split('\r\n');
+
+console.log(text[1])
+
+for(let i = 0; i < text.length; i++){
+
+}
+//console.log(text)
